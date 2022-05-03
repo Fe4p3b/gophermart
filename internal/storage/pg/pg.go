9a55f0a38018174d7106bca513133393bc7877a7
Almat Akhat <fabiannsailor@gmail.com>
2022-05-03 21:57:43 +0600

    testing

--- internal/storage/pg/pg.go
+++ internal/storage/pg/pg.go
@@ -165,9 +165,8 @@ func (p *pg) AddOrder(o *model.Order) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	fmt.Printf("AddOrder - %v", o)
 	sql := `INSERT INTO gophermart.orders(user_id, number, status, accrual, upload_date) VALUES ($1, $2, $3, $4, $5)`
-	if _, err := p.db.ExecContext(ctx, sql, o.UserID, o.Number, o.Status, o.Accrual, o.UploadDate); err != nil {
+	if _, err := p.db.ExecContext(ctx, sql, o.UserID, o.Number, o.Status.String(), o.Accrual, o.UploadDate); err != nil {
 		var pgErr *pgconn.PgError
 		if errors.As(err, &pgErr) && pgErr.Code == pgerrcode.UniqueViolation {
 			sql := `SELECT user_id FROM gophermart.orders WHERE number=$1`
--- migrations/001_init.sql
+++ migrations/001_init.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS gophermart.users(
 CREATE TABLE IF NOT EXISTS gophermart.orders(
     number varchar(255) PRIMARY KEY,
     user_id uuid NOT NULL REFERENCES gophermart.users(id),
-    status INT NOT NULL,
+    status VARCHAR(55) NOT NULL,
     accrual INT,
     upload_date TIMESTAMP WITH TIME ZONE,
     UNIQUE (number, user_id)
